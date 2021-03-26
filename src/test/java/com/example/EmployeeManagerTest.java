c995540fd7ace5ee9ffc26c2c5ce2f5e9de6ef2f
Johnny Olsen <johnny.olsen@iths.se>
2021-03-26 09:02:24 +0100

    Task 2 completed

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,53 @@
 package com.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.*;
+
+
+//Vi behöver tester för metoden payEmployees i klassen EmployeeManager i paketet
+// com.example. Klassen har 2 beroenden på interface som inte finns tillgängliga när vi kör unit
+// tester. Vi behöver alltså skapa testdoubles för dessa klasser för att kunna köra tester.
+// Gör minst ett test som använder en test double som du skapat själv. En test double
+// klass placerar vi I test/java foldern.
+// Gör minst ett test som använder mockito för att lösa problemet.
+// Skriv de tester som behövs för att få 100% kodtäckning av metoden.
 class EmployeeManagerTest {
+    @Test
+    void testPayEmployees() {
+        //TODO Arrange
+        BankService bankService = new TestBankService();
+        EmployeeRepository employeeRepository = new TestEmployeeRepository();
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+        // TODO Act
+        var actual = employeeManager.payEmployees();
+        // TODO Assert
+        assertThat(3).isEqualTo(actual);
+
+    }
+
+    @Test void testPayEmployeesThrowsAnException() {
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("1",1000));
+        var bankService = mock(BankService.class);
+        doThrow(RuntimeException.class).when(bankService).pay("1",1000);
+        var employeeRepository = mock(EmployeeRepository.class);
+        when(employeeRepository.findAll()).thenReturn(employeeList);
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+        employeeManager.payEmployees();
+
+        assertThat(employeeList.get(0).isPaid()).isFalse();
+        assertThat(employeeManager.payEmployees()).isEqualTo(0);
+    }
 
 }
+
---
+++ src/test/java/com/example/TestEmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.List;
+
+public class TestEmployeeRepository implements EmployeeRepository {
+    @Override
+    public List<Employee> findAll() {
+        Employee emp1 = new Employee("Test1", 5000.0);
+        Employee emp2 = new Employee("Test2", 6000.0);
+        Employee emp3 = new Employee("Test3", 7000.0);
+        return List.of(emp1,emp2, emp3);
+    }
+
+    @Override
+    public Employee save(Employee e) {
+        return null;
+    }
+}
